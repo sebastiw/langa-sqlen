b46b6a11d3c0eee002265df3175d452c79f4dd89
Sebastian Weddmark Olsson <visnae@gmail.com>
2020-04-23 13:04:15 +0200

    Preload instead of lookup

--- lib/langasql/resolver/tag.ex
+++ lib/langasql/resolver/tag.ex
@@ -1,15 +1,27 @@
 defmodule Langasql.Resolver.Tag do
   alias Langasql.Repo
 
+  alias Langasql.Ecto.User
   alias Langasql.Ecto.Tag
   alias Langasql.Ecto.Property
 
   def all(user, _attr, _schema) do
+    %User{properties: properties} = Repo.preload(user, [properties: :tags])
+
     {:ok,
-     Repo.all(Ecto.assoc(user, [:properties, :tags]))
+     properties
+     |> Enum.flat_map(fn (%Property{tags: tags} = prop) ->
+       # make a flat {%Tag,%Property} list
+       tags
+       |> Enum.map(fn (tag) ->
+         {tag, prop}
+       end)
+     end)
+     # Group all properties by tags instead
      |> Enum.group_by(
-     fn (%Tag{tag: tag}) -> tag end,
-     fn (%Tag{property_id: id}) -> Repo.get(Property, id) end)
+     fn ({%Tag{tag: tag},_}) -> tag end,
+     fn ({_,v}) -> v end)
+     # Make Absinthe happy (format)
      |> Enum.map(fn ({tag, props}) ->
        %{tag: tag, properties: props}
      end)}
