cf5193f0a527e9ad221ae13f771fd790b893769b
Sebastian Weddmark Olsson <visnae@gmail.com>
2020-04-26 22:35:00 +0200

    Remove fetching of static objects

--- lib/langasql_web/endpoint.ex
+++ lib/langasql_web/endpoint.ex
@@ -12,11 +12,11 @@ defmodule LangasqlWeb.Endpoint do
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-    at: "/",
-    from: :langasql,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+  # plug Plug.Static,
+  #   at: "/",
+  #   from: :langasql,
+  #   gzip: false,
+  #   only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
